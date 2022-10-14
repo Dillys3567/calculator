cdb28c20e5f2c00026bb8a12dadce632e17042b4
Dillys Annan <dillysannan@gmail.com>
2022-10-14 22:12:13 +0000

    buttons implementation

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
 
-import 'package:calculator/components/button.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class Calculator extends StatefulWidget {
   const Calculator({Key? key}) : super(key: key);
@@ -11,28 +11,115 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  String equation = "0";
+  String result = "0";
+  String expression = '';
+  double equationFontSize = 38.0;
+  double resultFontSize = 48.0;
+
+  buttonPressed(String buttonText) {
+    setState(() {
+      if (buttonText == 'C') {
+        equation = '0';
+        result = '0';
+        equationFontSize = 38.0;
+        resultFontSize = 48.0;
+        print('here');
+      } else if (buttonText == '⌫') {
+        equationFontSize = 48.0;
+        resultFontSize = 38.0;
+        equation = equation.substring(0, equation.length - 1);
+        if (equation == '') {
+          equation = '0';
+        }
+        print('here');
+      } else if (buttonText == '=') {
+        equationFontSize = 38.0;
+        resultFontSize = 48.0;
+        expression = equation;
+        expression = expression.replaceAll('÷', '/');
+        expression = expression.replaceAll('×', '*');
+        print(expression);
+        try {
+          Parser p = Parser();
+          Expression exp = p.parse(expression);
+          ContextModel conMod = ContextModel();
+          result = '${exp.evaluate(EvaluationType.REAL, conMod)}';
+        } catch (e) {
+          result = 'Error';
+        }
+      } else {
+        equationFontSize = 48.0;
+        resultFontSize = 38.0;
+        if (equation == '0') {
+          equation = buttonText;
+        } else {
+          equation += buttonText;
+          print('here');
+          print(buttonText);
+        }
+      }
+    });
+  }
+
+  Widget Button(String buttonText, double buttonHeight, Color buttonColor) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
+      color: buttonColor,
+      child: TextButton(
+        onPressed: () => buttonPressed(buttonText),
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(0),
+            side: const BorderSide(
+                color: Colors.white, width: 1, style: BorderStyle.solid),
+          ),
+        ),
+        child: Text(
+          buttonText,
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.normal,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Calculator'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.science_outlined),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.settings),
+          ),
+        ],
       ),
       body: Column(
         children: [
           Container(
             alignment: Alignment.centerRight,
             padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
-            child: Text(
-              '0',
-              style: TextStyle(fontSize: 38),
-            ),
+            child: Text(equation,
+                style: TextStyle(
+                  fontSize: equationFontSize,
+                )),
           ),
           Container(
             alignment: Alignment.centerRight,
             padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
             child: Text(
-              '0',
-              style: TextStyle(fontSize: 48),
+              result,
+              style: TextStyle(fontSize: resultFontSize),
             ),
           ),
           Expanded(child: Divider()),
@@ -45,97 +132,37 @@ class _CalculatorState extends State<Calculator> {
                   children: [
                     TableRow(
                       children: [
-                        Button(
-                          buttonText: 'C',
-                          buttonHeight: 1,
-                          buttonColor: Colors.redAccent,
-                        ),
-                        Button(
-                          buttonText: '⌫',
-                          buttonHeight: 1,
-                          buttonColor: Colors.blue,
-                        ),
-                        Button(
-                          buttonText: '÷',
-                          buttonHeight: 1,
-                          buttonColor: Colors.blue,
-                        ),
+                        Button('C', 1, Colors.redAccent),
+                        Button('⌫', 1, Colors.blue),
+                        Button('÷', 1, Colors.blue),
                       ],
                     ),
                     TableRow(
                       children: [
-                        Button(
-                          buttonText: '7',
-                          buttonHeight: 1,
-                          buttonColor: Colors.grey,
-                        ),
-                        Button(
-                          buttonText: '8',
-                          buttonHeight: 1,
-                          buttonColor: Colors.grey,
-                        ),
-                        Button(
-                          buttonText: '9',
-                          buttonHeight: 1,
-                          buttonColor: Colors.grey,
-                        ),
+                        Button('7', 1, Colors.grey),
+                        Button('8', 1, Colors.grey),
+                        Button('9', 1, Colors.grey),
                       ],
                     ),
                     TableRow(
                       children: [
-                        Button(
-                          buttonText: '4',
-                          buttonHeight: 1,
-                          buttonColor: Colors.grey,
-                        ),
-                        Button(
-                          buttonText: '5',
-                          buttonHeight: 1,
-                          buttonColor: Colors.grey,
-                        ),
-                        Button(
-                          buttonText: '6',
-                          buttonHeight: 1,
-                          buttonColor: Colors.grey,
-                        ),
+                        Button('4', 1, Colors.grey),
+                        Button('5', 1, Colors.grey),
+                        Button('6', 1, Colors.grey),
                       ],
                     ),
                     TableRow(
                       children: [
-                        Button(
-                          buttonText: '1',
-                          buttonHeight: 1,
-                          buttonColor: Colors.grey,
-                        ),
-                        Button(
-                          buttonText: '2',
-                          buttonHeight: 1,
-                          buttonColor: Colors.grey,
-                        ),
-                        Button(
-                          buttonText: '3',
-                          buttonHeight: 1,
-                          buttonColor: Colors.grey,
-                        ),
+                        Button('1', 1, Colors.grey),
+                        Button('2', 1, Colors.grey),
+                        Button('3', 1, Colors.grey),
                       ],
                     ),
                     TableRow(
                       children: [
-                        Button(
-                          buttonText: '.',
-                          buttonHeight: 1,
-                          buttonColor: Colors.grey,
-                        ),
-                        Button(
-                          buttonText: '0',
-                          buttonHeight: 1,
-                          buttonColor: Colors.grey,
-                        ),
-                        Button(
-                          buttonText: '00',
-                          buttonHeight: 1,
-                          buttonColor: Colors.grey,
-                        ),
+                        Button('.', 1, Colors.grey),
+                        Button('0', 1, Colors.grey),
+                        Button('00', 1, Colors.grey),
                       ],
                     )
                   ],
@@ -146,32 +173,16 @@ class _CalculatorState extends State<Calculator> {
                 child: Table(
                   children: [
                     TableRow(children: [
-                      Button(
-                        buttonText: 'x',
-                        buttonHeight: 1,
-                        buttonColor: Colors.blue,
-                      ),
+                      Button('×', 1, Colors.blue),
                     ]),
                     TableRow(children: [
-                      Button(
-                        buttonText: '-',
-                        buttonHeight: 1,
-                        buttonColor: Colors.blue,
-                      ),
+                      Button('-', 1, Colors.blue),
                     ]),
                     TableRow(children: [
-                      Button(
-                        buttonText: '+',
-                        buttonHeight: 1,
-                        buttonColor: Colors.blue,
-                      ),
+                      Button('+', 1, Colors.blue),
                     ]),
                     TableRow(children: [
-                      Button(
-                        buttonHeight: 2,
-                        buttonText: '=',
-                        buttonColor: Colors.redAccent,
-                      ),
+                      Button('=', 2, Colors.redAccent),
                     ]),
                   ],
                 ),
